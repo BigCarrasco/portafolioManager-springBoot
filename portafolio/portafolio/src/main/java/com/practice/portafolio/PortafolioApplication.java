7193d752128b5ae4b111b47035057c51417f041e
BigCarrasco <m.carrasco94@gmail.com>
2025-03-18 20:57:45 -0600

    Retryable y RestTemplate

--- portafolio/portafolio/src/main/java/com/practice/portafolio/PortafolioApplication.java
+++ portafolio/portafolio/src/main/java/com/practice/portafolio/PortafolioApplication.java
@@ -3,9 +3,11 @@ package com.practice.portafolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy // Habilita aspectos
+@EnableRetry // Habilita reintentos
 public class PortafolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortafolioApplication.class, args);
---
+++ portafolio/portafolio/src/main/java/com/practice/portafolio/controller/ApiController.java
@@ -0,0 +1,21 @@
+package com.practice.portafolio.controller;
+
+import com.practice.portafolio.service.ApiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Es para indicar que es un controlador REST
+@RequestMapping("/api") // Mapea las clases
+public class ApiController {
+    private final ApiService apiService;
+
+    public ApiController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping("/data")
+    public String getData() {
+        return apiService.obtenerDatosDesdeApi();
+    }
+}
