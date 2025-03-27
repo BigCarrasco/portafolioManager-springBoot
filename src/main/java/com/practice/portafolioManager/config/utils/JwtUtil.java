90ba6e3e83f8fd8a07e3e4cd8e621935ab1a4321
BigCarrasco <m.carrasco94@gmail.com>
2025-03-27 15:42:56 -0600

    RELEASE : AUTORIZATION WITH OAUTH & USER BASIC

--- src/main/java/com/practice/portafolioManager/config/utils/JwtUtil.java
+++ src/main/java/com/practice/portafolioManager/config/utils/JwtUtil.java
@@ -2,9 +2,12 @@ package com.practice.portafolioManager.config.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key; // diferencia entre Key y Key?
 import java.util.Date;
 import java.util.Map;
 
@@ -17,12 +20,43 @@ public class JwtUtil {
     @Value("${jwt.expiration}")
     private long expiration;
 
+    // Método para obtener la clave secreta como un objeto Key
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // Genera token
     public String generateToken(Map<String, Object> claims) {
         return Jwts.builder()
                 .setClaims(claims)
-                .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256) //get secret from a key object to sign in.,
                 .compact();
     }
+
+    // Valida token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // extra el email
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("email", String.class);
+    }
+
 }
